85f324635948099a1b78f911337627ce29edb31d
Michele Locati <michele@locati.it>
2021-05-19 17:51:19 +0200

    Version 4.3.0

--- controller.php
+++ controller.php
@@ -25,7 +25,7 @@ class Controller extends Package implements ProviderAggregateInterface
 
     protected $pkgHandle = 'acme';
 
-    protected $pkgVersion = '4.2.9';
+    protected $pkgVersion = '4.3.0';
 
     /**
      * {@inheritdoc}
