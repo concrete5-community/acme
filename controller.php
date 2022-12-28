e407cd52118d6b3aa5638e5bdca265ad5d7379c4
Michele Locati <michele@locati.it>
2022-12-28 17:42:40 +0100

    Version 4.4.0

--- controller.php
+++ controller.php
@@ -25,7 +25,7 @@ class Controller extends Package implements ProviderAggregateInterface
 
     protected $pkgHandle = 'acme';
 
-    protected $pkgVersion = '4.3.3';
+    protected $pkgVersion = '4.4.0';
 
     /**
      * {@inheritdoc}
