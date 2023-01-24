e31b6fd451af4896763a87fdcafa962a0e7d15cc
Michele Locati <michele@locati.it>
2023-01-24 15:06:20 +0100

    Add shim class

---
+++ src/shim.php
@@ -0,0 +1,70 @@
+<?php
+namespace phpseclib\Crypt
+{
+    class Hash {}
+    class RSA {}
+}
+namespace phpseclib\File
+{
+    class X509 {}
+}
+namespace phpseclib\Math
+{
+    class BigInteger {}
+}
+namespace phpseclib\Net
+{
+    class SFTP {}
+}
+namespace phpseclib\System\SSH
+{
+    class Agent {}
+}
+
+namespace phpseclib3\Crypt
+{
+    class Hash {}
+    class RSA {}
+}
+namespace phpseclib3\Crypt\Common
+{
+    class AsymmetricKey {}
+}
+namespace phpseclib3\Crypt\Common\Formats\Keys
+{
+    class OpenSSH {}
+    class PuTTY {}
+}
+namespace phpseclib3\Crypt\RSA
+{
+    class PrivateKey {}
+}
+namespace phpseclib3\File
+{
+    class X509 {}
+}
+namespace phpseclib3\Math
+{
+    class BigInteger {}
+}
+namespace phpseclib3\Net
+{
+    class SFTP {}
+}
+namespace phpseclib3\System\SSH
+{
+    class Agent {}
+}
+namespace Zend\Http\Client\Adapter
+{
+    class Curl {}
+}
+namespace Zend\Http\Header
+{
+    class Authorization {}
+}
+namespace Zend\Http
+{
+    class Request {}
+    class Response {}
+}
