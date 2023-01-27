0d838d61a2aebc308c773be73b3f0d0b264d3297
Michele Locati <michele@locati.it>
2023-01-27 15:13:36 +0100

    Version 6.0.2

--- controller.php
+++ controller.php
@@ -25,7 +25,7 @@ final class Controller extends Package implements ProviderAggregateInterface
 
     protected $pkgHandle = 'acme';
 
-    protected $pkgVersion = '6.0.1';
+    protected $pkgVersion = '6.0.2';
 
     /**
      * {@inheritdoc}
